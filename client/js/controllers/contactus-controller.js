a2def8eddc65a7327f589a8a13595d7208ba9a0a
Chris Molica <chrismolica@Chriss-MacBook-Pro.local>
2015-03-07 11:27:16 -0500

    updated contactt-us controller

--- client/js/controllers/contactus-controller.js
+++ client/js/controllers/contactus-controller.js
@@ -1,3 +1,37 @@
 /**
  * Created by chrismolica on 2/25/15.
  */
+angular.module('profile')
+    .controller('ProfileController',['$http', function($http){
+        var group = this;
+        this.group = group;
+        this.info = [];
+        $http.get('package.json').success(function(data){
+            group.info = data;
+        });
+    }]);
+angular.module('profile')
+    .controller("EmailSubscription", function(){
+        this.profile = {};
+        this.addUser = function(user){
+            this.profile = user;
+        };
+        this.removeUser = function(){
+            this.profile = {};
+        }
+    });
+angular.module('profile')
+    .controller("SignUpController", function(){
+        this.input = {};
+        this.addSignUp = function(user){
+            console.log('TEST');
+            this.input.createdOn = Date.now();
+            this.input.submited = true;
+            user.addUser(this.input);
+            //user = this.input;
+
+            //product.reviews.push(this.signup);
+            //this.input = {};
+        };
+
+    });
